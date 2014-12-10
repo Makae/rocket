6da7067e0236ead4d7a62cb42c1ca3d081a2abd3
tschm23 <matscha7@gmail.com>
2014-12-10 20:34:38 +0100

    Menus added

---
+++ Dokumente/Unity_Versionisierung/20141205_TSCHM23_Bombbox/GameMenu.js
@@ -0,0 +1,47 @@
+﻿var isQuit=false;
+var isStart=false;
+var isOptions=false;
+var isCredits=false;
+var isLoadGame=false;
+var isBackToMain=false;
+
+//Menu Color
+function OnMouseEnter(){
+	renderer.material.color=Color.red;
+}
+
+//menu color back to normal
+function OnMouseExit(){
+	//change text color
+	renderer.material.color=Color.yellow;
+}
+
+//Action on mouseclick
+function OnMouseUp(){
+	//is this quit
+	if(isQuit==true) {
+		//quit the game
+		Application.Quit();
+	}
+	if(isStart==true){
+		//load level
+		Application.LoadLevel("level1");
+	}
+	if(isCredits==true){
+		//open credits
+		Application.LoadLevel("Menu_Credits");
+	}
+	if(isOptions==true){
+		//open options
+		Application.LoadLevel("Menu_Options");
+	}
+	if(isLoadGame==true){
+		//open_load_game
+		Application.LoadLevel("Menu_Load");
+	}
+	if(isBackToMain==true){
+		//open_load_game
+		Application.LoadLevel("Menu_Main");
+	}
+	
+}
---
+++ Dokumente/Unity_Versionisierung/20141205_TSCHM23_Bombbox/RestartLevel.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RestartLevel : MonoBehaviour {
+	GameObject enemy;
+	private EnemyAI enemyai;
+	private LastPlayerSighting lastPlayerSighting;
+
+	void Start(){
+		//get the robot Object to access lastPlayer position and 
+		enemy = transform.parent.gameObject;
+
+
+	}
+
+	void Update(){
+		//Debug.Log ("XX Restert Level : position enemy");
+		//Debug.Log (enemy.transform.position.ToString());
+	}
+
+	//reset the position of the robot to the last position of playersighted
+	void resetPosition(){
+		lastPlayerSighting = GameObject.FindGameObjectWithTag(Tags.gameController).GetComponent<LastPlayerSighting>();
+		//GameObject gameController = GameObject.FindGameObjectWithTag<Tags.gameController>;
+		lastPlayerSighting.position = lastPlayerSighting.resetPosition;
+		//EnemyAI enemyai;
+	}
+
+
+
+
+	//if trigger is activated
+	void OnTriggerStay (Collider other)
+	{
+		if(other.gameObject.tag == Tags.player) //.. from player object
+		{
+			Debug.Log ("player in Deadzone!");
+			this.resetPosition();
+			//enemy.GetComponent<EnemyAI>();
+			//GameObject enemy
+			//GameObject enemy = GameObject.FindGameObjectWithTag(Tags.enemy);
+			//EnemyAI enemyai = enemy.GetComponent<EnemyAI>();
+			//if(enemyai.state == "Chasing"){
+			//	enemyai.state = "Patrolling";
+			//}
+			//GameObject enemy = GameObject.FindGameObjectWithTag(Tags.enemy);
+			//EnemyAI enemyai = this.enemy.GetComponent<EnemyAI>();
+			//enemyai.state("Patrolling");
+			//enemyai.state("Patrolling");
+
+			//anderer Ansatz
+			//GameObject enemy2 = GameObject.FindGameObjectWithTag(Tags.enemy);
+			//LastPlayerSighting lastPlayerSighting = enemy2.GetComponent<LastPlayerSighting>();
+			//lastPlayerSighting.position = lastPlayerSighting.resetPosition;
+
+			Application.LoadLevel("Main_Menu"); //go to Mainmenu
+			//Application.LoadLevel (Application.loadedLevel); //restart level
+		}
+	}
+}
